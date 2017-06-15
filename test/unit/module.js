05afd73058afa2ea4377fa024d97dc46b43ae174
Christoph Guttandin <chrisguttandin@media-codings.com>
2017-06-15 03:49:39 +0200

    apply linting rules

--- test/unit/module.js
+++ test/unit/module.js
@@ -44,11 +44,11 @@ describe('module', () => {
             };
         })(Worker);
 
-        const blob = new Blob([`
-            self.addEventListener('message', ({ data }) => {
+        const blob = new Blob([
+            `self.addEventListener('message', ({ data }) => {
                 self.postMessage(data);
-            });
-        `], { type: 'application/javascript' });
+            });`
+        ], { type: 'application/javascript' });
 
         metadataDetector = load(URL.createObjectURL(blob));
     });
